2d4a06f44c7cfe08f56b1d0fb9f47137ce7524b1
von Schappler <vonschappler_dev@outlook.com>
2024-10-18 09:20:35 -0300

    initial commit for class

---
+++ 04-pizza-app-ts/index.ts
@@ -0,0 +1,54 @@
+const menu = [
+  {
+    name: 'Margherita',
+    price: 8,
+  },
+  {
+    name: 'Pepperoni',
+    price: 10,
+  },
+  {
+    name: 'Hawaiian',
+    price: 10,
+  },
+  {
+    name: 'Veggie',
+    price: 9,
+  },
+];
+
+let cashInRegister = 100;
+let orderId = 1;
+
+const orderQueue = [];
+
+const addNewPizza = (newPizza) => {
+  menu.push(newPizza);
+  return menu;
+};
+
+const placeOrder = (pizzaName) => {
+  const pizza = menu.find((menuItem) => menuItem.name === pizzaName);
+  if (!pizza)
+    return `There is no ${pizzaName} on the menu, please consider adding this before placing this order.`;
+  cashInRegister += pizza.price;
+  const newOrder = { id: orderId++, pizza, status: 'ordered' };
+  orderQueue.push(newOrder);
+  console.log(orderQueue, cashInRegister);
+  return newOrder;
+};
+
+const completeOrder = (id) => {
+  const order = orderQueue.find((order) => order.id === id);
+  order.status = 'completed';
+  return order;
+};
+
+addNewPizza({ name: 'Chicken Bacon Ranch', price: 12 });
+addNewPizza({ name: 'BBQ Chicken', price: 12 });
+addNewPizza({ name: 'Spicy Sausage', price: 11 });
+
+placeOrder('Chicken Bacon Ranch');
+completeOrder('1');
+
+console.log({ menu, cashInRegister, orderQueue });
