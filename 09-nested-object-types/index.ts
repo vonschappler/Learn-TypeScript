ca187a4fd17e98af3dd981a7d5118c2a34ba45bc
von Schappler <vonschappler_dev@outlook.com>
2024-10-18 13:55:31 -0300

    Final code for class

--- 09-nested-object-types/index.ts
+++ 09-nested-object-types/index.ts
@@ -2,22 +2,34 @@ type Food = string;
 
 let favoriteFoot: Food = 'pizza';
 
+type Address = {
+  street: string;
+  city: string;
+  country: string;
+};
+
 type Person = {
   age: number;
   name: string;
   isAdult: boolean;
+  address: Address;
 };
 
 let person1: Person = {
   name: 'von',
   age: 40,
   isAdult: true,
+  address: {
+    street: 'Some street',
+    city: 'Some city',
+    country: 'Some country',
+  },
 };
 
 let person2: Person = {
   name: 'Schappler',
   age: 40,
-
-  // this will trigger a potential error, since the person object does not expect the porperty "isadult", but "isAdult" instead
-  isadult: true,
+  isAdult: true,
+  // this line will trigger an error because the address does not match the type address declared
+  address: 'Street Address',
 };
