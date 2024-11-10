9a5d77649065a4855092c5512b9ec874690fb1a5
von Schappler <vonschappler_dev@outlook.com>
2024-11-10 09:35:05 -0300

    Initial commit for class

---
+++ 19-return-types/index.ts
@@ -0,0 +1,29 @@
+type UserRole = 'guest' | 'member' | 'admin';
+
+type User = {
+  username: string;
+  role: UserRole;
+};
+
+const users: User[] = [
+  {
+    username: 'user1',
+    role: 'admin',
+  },
+  {
+    username: 'user2',
+    role: 'member',
+  },
+  {
+    username: 'user3',
+    role: 'guest',
+  },
+];
+
+const fetchUserDetails = (username: string) => {
+  const user = users.find((user) => user.username === username);
+  if (!user) {
+    throw new Error(`User owht username ${username} not found`);
+  }
+  return user;
+};
