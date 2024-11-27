b8c65d730cda5745c88bf0d673902acb59d99b67
von Schappler <vonschappler_dev@outlook.com>
2024-11-27 10:33:46 -0300

    Final code for class

--- 23-utility-types/index.ts
+++ 23-utility-types/index.ts
@@ -4,32 +4,45 @@ type User = {
   role: 'member' | 'contributor' | 'admin';
 };
 
+type UpdateUser = Partial<User>;
+type NewUser = Omit<User, 'id'>;
+
+let newUserId: number = 1;
+
 const users: User[] = [
   {
-    id: 1,
+    id: newUserId++,
     username: 'john_doe',
     role: 'member',
   },
   {
-    id: 2,
+    id: newUserId++,
     username: 'jane_doe',
     role: 'contributor',
   },
   {
-    id: 3,
+    id: newUserId++,
     username: 'alice_doe',
     role: 'admin',
   },
   {
-    id: 4,
+    id: newUserId++,
     username: 'charlie_doe',
     role: 'member',
   },
 ];
 
-const updateUser = (id: any, updates: any) => {};
+const updateUser = (id: number, updates: UpdateUser): void => {
+  const foundUser = users.find((user) => user.id === id);
+  if (!foundUser) return console.log(`There is no user with id ${id}`);
+  Object.assign(foundUser, updates);
+};
 
-const addUser = (newUsers: any): User => {};
+const addUser = (newUser: NewUser): User => {
+  const user: User = { id: newUserId++, ...newUser };
+  users.push(user);
+  return user;
+};
 
 updateUser(1, { username: 'new_john_doe' });
 updateUser(4, { role: 'contributor' });
