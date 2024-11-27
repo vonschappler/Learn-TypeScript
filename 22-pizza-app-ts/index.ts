a5f014ef32474cb5a32aea5ae3d9bb6530ee9612
von Schappler <vonschappler_dev@outlook.com>
2024-11-27 08:57:42 -0300

    Final code for class

--- 22-pizza-app-ts/index.ts
+++ 22-pizza-app-ts/index.ts
@@ -10,35 +10,36 @@ type Order = {
   status: 'ordered' | 'completed';
 };
 
+let cashInRegister: number = 100;
+let orderId: number = 1;
+let pizzaId: number = 1;
+const orderQueue: Order[] = [];
+
 const menu: Pizza[] = [
   {
-    id: 1,
+    id: pizzaId++,
     name: 'Margherita',
     price: 8,
   },
   {
-    id: 2,
+    id: pizzaId++,
     name: 'Pepperoni',
     price: 10,
   },
   {
-    id: 3,
+    id: pizzaId++,
     name: 'Hawaiian',
     price: 10,
   },
   {
-    id: 4,
+    id: pizzaId++,
     name: 'Veggie',
     price: 9,
   },
 ];
 
-let cashInRegister: number = 100;
-let orderId: number = 1;
-
-const orderQueue: Order[] = [];
-
 const addNewPizza = (newPizza: Pizza): void => {
+  newPizza.id = pizzaId++;
   menu.push(newPizza);
 };
 
@@ -82,9 +83,9 @@ export const getPizzaDetail = (
   }
 };
 
-addNewPizza({ id: 5, name: 'Chicken Bacon Ranch', price: 12 });
-addNewPizza({ id: 6, name: 'BBQ Chicken', price: 12 });
-addNewPizza({ id: 7, name: 'Spicy Sausage', price: 11 });
+addNewPizza({ name: 'Chicken Bacon Ranch', price: 12 });
+addNewPizza({ name: 'BBQ Chicken', price: 12 });
+addNewPizza({ name: 'Spicy Sausage', price: 11 });
 
 placeOrder('Chicken Bacon Ranch');
 completeOrder(1);
