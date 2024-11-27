021103e8efdbc5d9534d3688063845525dd56b0f
von Schappler <vonschappler_dev@outlook.com>
2024-11-27 11:31:35 -0300

    Initial commit for class

---
+++ 25-generics/index.ts
@@ -0,0 +1,17 @@
+type Student = {
+  name: string;
+  grade: number;
+};
+
+const scores: number[] = [14, 21, 33, 42, 59];
+const favs: string[] = ['chocolate', 'dogs', 'cuddling'];
+const students: Student[] = [
+  { name: 'Student A', grade: 10 },
+  { name: 'Student B', grade: 9 },
+  { name: 'Student C', grade: 7 },
+  { name: 'Student D', grade: 3 },
+  { name: 'Student E', grade: 2 },
+  { name: 'Student F', grade: 5 },
+];
+
+const getLastItem = (array) => {};
